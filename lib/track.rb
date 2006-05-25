b862e652f86a63018dc0450c6b9c0cb663c338ca
ogd <ogd@euphrosyne.local>
2006-05-24 18:56:57 -0700

    Added support for max_disc_number to track, where it really doesn't belong. Will probably come right out again after I finish refactoring into DAOS.

--- lib/track.rb
+++ lib/track.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'track_metadata')
 
 class Track
   attr_accessor :album_name, :name, :artist_name
-  attr_accessor :disc_number, :sequence, :max_sequence
+  attr_accessor :disc_number, :max_disc_number, :sequence, :max_sequence
   attr_accessor :genre, :comment, :encoder, :musicbrainz_artist_id
   
   def initialize(path)
@@ -16,6 +16,7 @@ class Track
     @sequence = id3v2_metadata.sequence || filename_metadata.sequence.to_i
     @max_sequence = id3v2_metadata.max_sequence
     @disc_number = path_metadata.disc_number.to_i || id3v2_metadata.disc_number
+    @max_disc_number = id3v2_metadata.max_disc_number
     @genre = id3v2_metadata.genre
     @comment = id3v2_metadata.comment
     @encoder = id3v2_metadata.encoder
