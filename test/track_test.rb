4f218619c131c53dbe90971713ea87ee6044b57e
ogd <ogd@euphrosyne.local>
2006-05-24 18:56:10 -0700

    Completed test case.

--- test/track_test.rb
+++ test/track_test.rb
@@ -7,5 +7,11 @@ class TrackTest < Test::Unit::TestCase
     track = Track.new '../../mp3info/sample-metadata/zovietfrance/Popular Soviet Songs And Youth Music disc 3/zovietfrance - Popular Soviet Songs And Youth Music - 07 - Charm Aliso.mp3'
     assert_equal ':zoviet*france:', track.artist_name
     assert_equal 'Popular Soviet Songs And Youth Music', track.album_name
+    assert_equal 'Experimental', track.genre
+    assert_equal 7, track.sequence
+    assert_equal 9, track.max_sequence
+    assert_equal 3, track.disc_number
+    assert_equal 3, track.max_disc_number
+    assert_equal '(ID3v1 Comment) [XXX]: RIPT with GRIP', track.comment
   end
 end
