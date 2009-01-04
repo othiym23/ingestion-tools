14fa67a1db37ce84c3eef4fe9d048bedd1d5089f
ogd <ogd@aoaioxxysz.net>
2009-01-04 13:52:54 -0800

    Rails framework: * [CHANGED] Added default configuration for latest version of Rails.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,3 +8,12 @@ config.cache_classes = true
 
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching             = false
+
+# Tell ActionMailer not to deliver emails to the real world.
+# The :test delivery method accumulates sent emails in the
+# ActionMailer::Base.deliveries array.
+config.action_mailer.delivery_method = :test
