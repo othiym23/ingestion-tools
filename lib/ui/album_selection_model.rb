cef1f03c322cd18547f524d9e6be4669ea7e2cfc
ogd <ogd@aoaioxxysz.net>
2009-01-04 15:02:10 -0800

    management console: * [NEW] Introduced a pending archival queue to enable batch processing   of albums. * [CHANGED] Added much more logging. * [CHANGED] Refined selecting an album from the list, narrowing the list, and   resetting the narrowed list and selected album when changing modes. * [FIXED] Changed status line accessor to be write-only.

--- lib/ui/album_selection_model.rb
+++ lib/ui/album_selection_model.rb
@@ -2,25 +2,43 @@ require 'ui/selection_model'
 require 'adaptor/euterpe_dashboard_factory'
 
 class AlbumSelectionModel < SelectionModel
-  attr_accessor :status
-  attr_writer :album_loader
+  attr_writer :status
+  attr_reader :archiving_queue
   
-  def initialize
+  def initialize(album_loader, logger)
+    @album_loader = album_loader
+    @logger = logger
+
     @narrowing_terms = []
+    @archiving_queue = []
   end
   
   def selected=(album)
     if album
       @status.message = "reloading metadata for #{album.artist_name} - #{album.name}"
+      @logger.debug('selected=') { "reloading metadata for #{album.artist_name} - #{album.name}" }
+      
       metadata_album = AlbumDao.reload_album_from_files(album)
       metadata_album.cached_album = album.cached_album
+      metadata_album.original_album = album
+      @logger.debug('selected=') { "after reloading have #{metadata_album.artist_name} - #{metadata_album.name}" }
     end
     
     super(metadata_album)
+    @logger.debug('selected=') { "selected is now #{selected.artist_name} - #{selected.name}"} if @selected
+    metadata_album
   end
   
   def narrowed_list
-    @narrowed_list || @list
+    # if the underlying list has been modified, we need to refresh the narrowed list
+    if @narrowed_list && (@narrowed_list.size > @list.size)
+      @narrowed_list = @list
+      MY_LOGGER.debug("narrowed list refreshed")
+    end
+    
+    @narrowed_list = nil if @narrowed_list && (@narrowed_list.size == 0)
+    
+    (@narrowed_list || @list || []) - (@archiving_queue || [])
   end
   
   def narrow(term)
@@ -29,8 +47,14 @@ class AlbumSelectionModel < SelectionModel
       
       changed
       if @list.nil?
+        @narrowing_terms = []
+        @narrowed_list = nil
+        
         raw_list = narrow_albums([], term)
+        @logger.debug('narrow') { "got from narrowing #{raw_list} with a size of #{raw_list ? raw_list.size : 0}"}
+        
         if raw_list && 1 == raw_list.size
+          @logger.debug('narrow') { "single result found, setting selection"}
           self.selected = raw_list.first
         else
           @list = raw_list
@@ -71,13 +95,32 @@ class AlbumSelectionModel < SelectionModel
     changed
     notify_observers
   end
+
+  def will_archive(album)
+    @archiving_queue.push(album)
+  end
   
+  def has_archived(album)
+    @archiving_queue.delete(album)
+  end
+
   private
   
   def narrow_albums(albums, search_term)
     if [] == albums
       if search_term && '' != search_term.strip
-        @album_loader.find_generously(search_term)
+        @logger.debug('narrow_albums') { "searching metadata cache for '#{search_term}'"}
+        generous_results = @album_loader.find_generously(search_term)
+        @logger.debug('narrow_albums') { "metadata cache returns cached IDs #{generous_results.collect{|result| result.cached_album.id}.join(', ')}"}
+
+        if @archiving_queue.size > 0
+          @logger.debug('narrow_albums') { "archiving queue contains cached IDs #{@archiving_queue.collect{|result| result.cached_album.id}.join(', ')}"}
+          curried_results = generous_results.reject { |result| @archiving_queue.detect { |enqueued| enqueued.cached_album.id == result.cached_album.id } }
+          @logger.debug('narrow_albums') { "difference of queue and results is #{curried_results.collect{|result| result.cached_album.id}.join(', ')}"}
+          curried_results
+        else
+          generous_results
+        end
       end
     else
       albums.select do |album|
